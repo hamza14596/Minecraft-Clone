c3c4952508d3ec1b420cec3ac962465def3befdd
hamza <hmzuuu87@gmail.com>
2025-09-21 10:17:07 +0500

    added alot of stuff incuding, blocks, plain, sky etc.

--- main.py
+++ main.py
@@ -1,43 +1,100 @@
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
 
-class Test_cube(Entity):
-    def __init__(self):
-        super().__init__(
-            model = 'cube',
-            color = color.white,
-            texture = 'white_cube',
-            rotation = Vec3(45,45,45))   
 
-class Test_button(Button):
-    def __init__(self):
+app = Ursina()
+
+
+grass_texture = load_texture('assets/grass_block.png')
+stone_texture = load_texture('assets/stone_block.png')
+brick_texture = load_texture('assets/brick_block.png')
+dirt_texture = load_texture('assets/dirt_block.png')
+
+arm_texture = load_texture('assets/arm_texture.png')
+punch_sound = Audio('assets/punch_sound',loop = False, autoplay = False)
+
+sky_texture = load_texture('assets/skybox.png')
+block_pick = 1
+
+window.fps_counter.enabled = False
+window.entity_counter.enabled = False
+window.collider_counter.enabled = False
+
+def update():
+    global block_pick
+    if held_keys['left mouse'] or held_keys['right mouse']:
+        hand.active()
+        
+    else:
+        hand.passive()  
+
+    if held_keys['1']: block_pick = 1
+    if held_keys['2']: block_pick = 2
+    if held_keys['3']: block_pick = 3
+    if held_keys['4']: block_pick = 4
+
+
+class Voxel(Button):
+    def __init__(self, position = (0,0,0), texture = grass_texture):
         super().__init__(
             parent = scene,
-            model = 'cube',
-            texture = 'brick',
-            color = color.blue,
-            highlight_color = color.red,
-            pressed_color = color.lime,)
+            position = position,
+            model = 'assets/block',
+            origin_y = 0.5,
+            texture = texture,
+            color = color.white,
+            scale = 0.5)
         
     def input(self,key):
         if self.hovered:
             if key == 'left mouse down':
-                print('button pressed')
-                self.position = (0,0,0)
-            if key == 'left mouse up':
-                self.position = (0,0,0.2)
+                punch_sound.play()
+                if block_pick == 1: voxel = Voxel(position = self.position + mouse.normal, texture = grass_texture)
+                if block_pick == 2: voxel = Voxel(position = self.position + mouse.normal, texture = stone_texture)
+                if block_pick == 3: voxel = Voxel(position = self.position + mouse.normal, texture = brick_texture)
+                if block_pick == 4: voxel = Voxel(position = self.position + mouse.normal, texture = dirt_texture)
+                
 
+            if key == 'right mouse down':
+                punch_sound.play()
+                destroy(self)
+
+
+class sky(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = scene,
+            model = 'sphere',
+            texture = sky_texture,
+            scale = 200,
+            double_sided = True
+        )
+
+class Hand(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = camera.ui,
+            model = 'assets/arm',
+            texture = arm_texture,
+            scale = 0.2,
+            rotation = Vec3(150,-10,0),
+            position = Vec2(0.4,-0.6)
+        )
+
+
+    def active(self):
+        self.position = Vec2(0.3,-0.5)
         
 
-def update():
-    if held_keys['a']:
-        test_square.x -= 4 * time.dt
+    def passive(self):
+        self.position = Vec2(0.4,-0.6)
 
-app = Ursina()
+for z in range(20):
+    for x in range(20):
+        voxel = Voxel(position = (x,0,z))
 
-test_square = Entity(model = 'quad', color = color.red, scale= (1,4), position = (5,4))
-sans = Entity(model = 'quad', texture = ('assets/sans.png'))
-
-
-test_button = Test_button()
+player = FirstPersonController()
+sky = sky()
+hand = Hand()
 
 app.run()
